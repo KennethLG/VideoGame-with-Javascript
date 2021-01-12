1c4d8ebe770f52d6c907d4791885599421f21949
KennethLG <orsonleiziaga@gmail.com>
2021-01-11 19:49:51 -0500

    electron configured

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow, Menu} = require("electron");
+const url = require("url");
+const path = require("path");
+
+if (process.env.NODE_ENV !== "production") {
+	require("electron-reload")(__dirname, {
+		electron: path.join(__dirname, "../node_modules", ".bin", "electron")
+	});
+}
+
+let mainWindow;
+
+app.on("ready", ()=> {
+	mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, "views/index.html"),
+		protocol: "file",
+		slashes: true,
+	}));
+
+	const mainMenu = Menu.buildFromTemplate([]);
+	Menu.setApplicationMenu(mainMenu);
+});
